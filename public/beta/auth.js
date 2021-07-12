7ed71e87f5b2461bfca6f0b164047a8060e71ab4
lumeyisnotyou <46928172+lumeyisnotyou@users.noreply.github.com>
2021-07-11 22:15:00 -0400

    trying auth

---
+++ public/beta/auth.js
@@ -0,0 +1,16 @@
+
+function loginUser(form) {
+var email = document.getElementById("email").value;
+var password = document.getElementById("pass").value;
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    alert("wow!");
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
