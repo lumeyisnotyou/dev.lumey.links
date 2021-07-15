546ac53723b621bf87cf6e500d39d66d2f7a91f4
lumeyisnotyou <46928172+lumeyisnotyou@users.noreply.github.com>
2021-07-15 16:49:49 -0400

    yay

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 $target_dir = "./";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["filePond"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
