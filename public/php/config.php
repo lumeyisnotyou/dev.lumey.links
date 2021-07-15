ed620fbd52a5ce52f908d2d09e9d7c20f0ab35a3
lumeyisnotyou <46928172+lumeyisnotyou@users.noreply.github.com>
2021-07-15 17:23:51 -0400

    nonsense

--- public/php/config.php
+++ public/php/config.php
@@ -4,9 +4,9 @@
 const ENTRY_FIELD = array('filepond');
 
 // where to write files to
-const TRANSFER_DIR = __DIR__ . '/tmp';
-const UPLOAD_DIR = __DIR__ . '/uploads';
-const VARIANTS_DIR = __DIR__ . '/variants';
+const TRANSFER_DIR = '/tmp/';
+const UPLOAD_DIR = '/uploads/';
+const DATABASE_FILE = './database.json';
 
 // name to use for the file metadata object
 const METADATA_FILENAME = '.metadata';
@@ -14,6 +14,3 @@ const METADATA_FILENAME = '.metadata';
 // this automatically creates the upload and transfer directories, if they're not there already
 if (!is_dir(UPLOAD_DIR)) mkdir(UPLOAD_DIR, 0755);
 if (!is_dir(TRANSFER_DIR)) mkdir(TRANSFER_DIR, 0755);
-
-// this is optional and only needed if you're doing server side image transforms, if images are transformed on the clients, this can stay commented
-// require_once('config_doka.php');
