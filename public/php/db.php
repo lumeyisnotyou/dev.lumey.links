d5068bc0891ee2c21d65070f1aef283561538a41
lumeyisnotyou <46928172+lumeyisnotyou@users.noreply.github.com>
2021-07-16 11:16:21 -0400

    Update db.php

--- public/php/db.php
+++ public/php/db.php
@@ -14,22 +14,12 @@ die("oh no, i couldn't connect to MySQL! here's why: " . $conn->connect_error);
 $dbjson = file_get_contents('php://input');
 $dbobj = json_decode($dbjson);
 
-$doesUserID = "IF (EXISTS (SELECT *
-   FROM INFORMATION_SCHEMA.TABLES
-   WHERE TABLE_SCHEMA = 'dbo'
-   AND TABLE_NAME = '" . $dbobj->uniqueID . "'))
-   BEGIN
-      PRINT 'TRUE'
-   END;
-ELSE
-   BEGIN
-      PRINT 'FALSE'
-   END; ";
+$doesUserID = mysql_query('select 1 from ' . $dbobj->userID . ' LIMIT 1');
 
 $addImageUpload = "INSERT INTO " . $dbobj->userID . " (uniqueID, fileName)
 VALUES('" . $dbobj->uniqueID . "', '" . $dbobj->fileName . "')";
 
-if ($conn->query($doesUserID) === 'TRUE') {
+if ($conn->query($doesUserID) === TRUE) {
   echo "Found Table";
   if ($conn->query($addImageUpload) === TRUE) {
   echo "Created Image Upload!";
