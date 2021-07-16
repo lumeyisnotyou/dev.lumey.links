40fc1f4ac743604b36267b0e8465211c44c9b051
lumeyisnotyou <46928172+lumeyisnotyou@users.noreply.github.com>
2021-07-16 10:46:17 -0400

    Update db.php

--- public/php/db.php
+++ public/php/db.php
@@ -1,9 +1,26 @@
 <?php
+// db n' stuff
+$servername = 'localhost';
+$username = 'PHP';
+$password = 'd&(SYA&FDT6HA^ATS6ts&GHsf*^A%sa$&'
+$db = "ugc_images";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+die("oh no, i couldn't connect to MySQL! here's why: " . $conn->connect_error);
+}
 
 $dbjson = file_get_contents('php://input');
+$dbobj = json_decode($dbjson);
 
-if (file_put_contents("database.json", $dbjson))
-	echo "Succesfully saved user's upload";
-else
-	echo "oh no";
+$doesUserID = "SELECT count(*)
+FROM information_schema.TABLES
+WHERE (TABLE_SCHEMA = 'ugc_images') AND (TABLE_NAME = '" . $dbobj->userID . "')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Found Table";
+} else {
+  echo "No: " . $conn->error;
+}
 
