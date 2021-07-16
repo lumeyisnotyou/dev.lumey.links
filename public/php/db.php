517aa13f6ae17468e9ba8c8d55067008aaf5d662
lumeyisnotyou <46928172+lumeyisnotyou@users.noreply.github.com>
2021-07-16 10:17:55 -0400

    Update db.php

--- public/php/db.php
+++ public/php/db.php
@@ -3,7 +3,7 @@
 $dbjson = file_get_contents('php://input');
 
 if (file_put_contents("database.json", $dbjson))
-	echo "Succesfully saved user's upload"
+	echo "Succesfully saved user's upload";
 else
-	echo "oh no"
+	echo "oh no";
 
