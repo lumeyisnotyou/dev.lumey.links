7576165cba9939893c5e842f9987c104df447696
lumeyisnotyou <46928172+lumeyisnotyou@users.noreply.github.com>
2021-07-16 11:30:28 -0400

    Update db.php

--- public/php/db.php
+++ public/php/db.php
@@ -30,8 +30,8 @@ if ($conn->query($doesUserID) !== FALSE) {
 } else {
  	$makeTable = "CREATE TABLE " . $dbobj->userID . " (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		uniqueID VARCHAR(30) NOT NULL,
-	fileName VARCHAR(30) NOT NULL,
+		uniqueID NOT NULL,
+	fileName NOT NULL,
 	reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 	)";
 		if ($conn->query($makeTable) === TRUE) {
